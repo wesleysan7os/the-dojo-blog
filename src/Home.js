96bf3dd9be3638ac326eacdbd3fc61f298513acc
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2021-04-30 18:04:50 -0300

    Using promps

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 function Home() {
-
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -10,14 +10,10 @@ function Home() {
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      ))} 
+      <BlogList blogs={blogs} title="All Blogs!"/>
     </div>
   );
 }
  
 export default Home;
+
