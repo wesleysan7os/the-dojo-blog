d4adbfd7e08fd0ce02c1d8b8382408adf31780ca
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2021-05-01 16:46:59 -0300

    useEffect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,9 +4,12 @@ function useFetch(url) {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then(response => {
           if(!response.ok) {
             throw Error('Could not fetch the data for that resource.');
@@ -19,10 +22,17 @@ function useFetch(url) {
           setError(null);
         })
         .catch(err => {
-          setError(err.message);
-          setIsLoading(false);
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+            setIsLoading(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+
+    return () => abortCont.abort();
+
   }, [url]); 
 
   return { data, isLoading, error };
