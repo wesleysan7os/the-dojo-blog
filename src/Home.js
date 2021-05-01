cb4216507834bf18cfa51e6622e508e420f0c844
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2021-05-01 13:43:03 -0300

    Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -3,18 +3,22 @@ import BlogList from './BlogList';
 
 function Home() {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
       .then(response => response.json())
       .then(data => {
-        console.log(data);
         setBlogs(data);
+        setIsLoading(false);
       });
+    }, 1000);
   }, []); 
 
   return (
     <div className="home">
+      {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
     </div>
   );
